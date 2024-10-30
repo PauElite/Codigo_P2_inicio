ca435d8f594852ce6a0b7080f475331e51c22c1a
PauElite <paujimenezmartinez@hotmail.com>
2024-10-30 13:20:37 +0100

    Print = 0

--- src/mh.c
+++ src/mh.c
@@ -9,7 +9,7 @@
 #include "../include/mh.h"
 
 #define MUTATION_RATE 0.15
-#define PRINT 1
+#define PRINT 0
 
 int aleatorio(int n) {
     return (rand() % n);  // genera un numero aleatorio entre 0 y n-1

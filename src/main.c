032253cd0a5cdb407a066caef25a3f836cc8e7ee
PauElite <paujimenezmartinez@hotmail.com>
2024-10-30 14:26:36 +0100

    Arreglado MPI_Bcast

--- src/main.c
+++ src/main.c
@@ -5,64 +5,98 @@
 #include <mpi.h>
 
 #include "../include/io.h"
+#define TIME 1
 
 extern double aplicar_mh(const double *, int, int, int, int, double, int *);
 
-static double mseconds() {
+static double mseconds()
+{
     struct timeval t;
     gettimeofday(&t, NULL);
     return t.tv_sec * 1000 + t.tv_usec / 1000;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     MPI_Init(&argc, &argv); // Inicializar entorno MPI
 
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    int n = 0;
+    int m = 0;
+    int n_gen = 0;
+    int tam_pob = 0;
+    double m_rate = 0.0;
+
     // Check Number of Input Args
-    if (argc < 5) {
-        if (rank == 0) {
+    if (rank == 0)
+    {
+        if (argc < 5)
+        {
             fprintf(stderr, "Ayuda:\n");
             fprintf(stderr, "  mpirun -np <n_procesos> ./programa n m nGen tamPob mRate\n");
+            MPI_Finalize();
+            return (EXIT_FAILURE);
         }
-        MPI_Finalize();
-        return (EXIT_FAILURE);
+
+        n = atoi(argv[1]);
+        m = atoi(argv[2]);
+        n_gen = atoi(argv[3]);
+        tam_pob = atoi(argv[4]);
+        m_rate = atof(argv[5]);
+
+        // Check that 'm' is less than 'n'
+        assert(m < n);
+
+        // Generate matrix D with distance values among elements
+        // double *d = read_distances(n);
+    }
+    // printf("\nSoy el proceso %d y estos son mis datos: n=%d\tm=%d\tn_gen=%d\ttam_pob=%d\tm_rate=%.2lf", rank, n, m, n_gen, tam_pob, m_rate);
+    MPI_Bcast(&n, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&n_gen, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&tam_pob, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&m_rate, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    // printf("\nSoy el proceso %d y estos son mis datos: n=%d\tm=%d\tn_gen=%d\ttam_pob=%d\tm_rate=%.2lf\n", rank, n, m, n_gen, tam_pob, m_rate);
+
+    double *d = NULL;
+    if (rank == 0)
+    {
+        d = read_distances(n);
+    }
+    else
+    {
+        d = (double *)malloc(((n * n - n) / 2) * sizeof(double));
     }
 
-    int n = atoi(argv[1]);
-    int m = atoi(argv[2]);
-    int n_gen = atoi(argv[3]);
-    int tam_pob = atoi(argv[4]);
-    double m_rate = atof(argv[5]);
-
-    // Check that 'm' is less than 'n'
-    assert(m < n);
-
-    // Generate matrix D with distance values among elements
-    double *d = read_distances(n);
-
+    MPI_Bcast(d, (n * n - n) / 2, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     // Allocate memory for output data
     int *sol = (int *)malloc(m * sizeof(int));
 
-    #ifdef TIME
-        MPI_Barrier(MPI_COMM_WORLD);
-        double ti = mseconds();
-    #endif
+#ifdef TIME
+    MPI_Barrier(MPI_COMM_WORLD);
+    double ti = mseconds();
+#endif
 
     // Call Metaheuristic
-    double value = aplicar_mh(d, n, m, n_gen, tam_pob / size, m_rate, sol);
+    double value;
+    if (rank != 0)
+    {
+        // value = aplicar_mh(d, n, m, n_gen, tam_pob / size, m_rate, sol);
+    }
 
     // Gather results at root process
     double global_value;
     MPI_Reduce(&value, &global_value, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
 
-    if (rank == 0) {
-        #ifdef TIME
-            double tf = mseconds();
-            printf("Execution Time: %.2lf sec\n", (tf - ti) / 1000);
-        #endif
+    if (rank == 0)
+    {
+#ifdef TIME
+        double tf = mseconds();
+        printf("Execution Time: %.2lf sec\n", (tf - ti) / 1000);
+#endif
         printf("Best Fitness Value: %.2lf\n", global_value);
     }
 
